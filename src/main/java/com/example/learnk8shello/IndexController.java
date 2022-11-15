b21411a2e4149e56cd733f3bdfb904aa4c3e4a9e
liziyi0914 <liziyi0914@gmail.com>
2022-11-16 02:48:11 +0800

    add IndexController.java

--- src/main/java/com/example/learnk8shello/IndexController.java
+++ src/main/java/com/example/learnk8shello/IndexController.java
@@ -1,2 +1,22 @@
-package com.example.learnk8shello;public class IndexController {
+package com.example.learnk8shello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello from Spring Boot!";
+    }
+
+    @GetMapping("/{name}")
+    public String index(@PathVariable String name) {
+        return "Hello "+name+"!";
+    }
+
 }
